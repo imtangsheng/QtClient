4b646f43f4d8073781c610d0a620d936bc1fa308
tangsheng <imtangsheng@outlook.com>
2024-04-01 18:04:43 +0800

    视频播放插件模块整理

--- MediaPlayer/mediaplayer.cpp
+++ MediaPlayer/mediaplayer.cpp
@@ -1,8 +1,13 @@
 #include "mediaplayer.h"
+#include "AppOS.h"
+
+QSettings AppSettings("config/MediaPlayer.ini",QSettings::IniFormat);
+QJsonObject AppJson;
 
 MediaPlayer::MediaPlayer(QObject *parent) : QObject(parent)
 {
-    widgets = new MediaWidgets(nullptr);
+    widgets = new MediaWidgets;
+    videoWindow = new VideoWindow(widgets);
     init();
 }
 
@@ -11,28 +16,43 @@ void MediaPlayer::init()
     ObjectName = widgets->objectName();
     WindowIcon = QIcon(":/asset/titler/MediaPlayer.svg");
     WindowTitle = tr("视频回放");
-    connect(widgets,&MediaWidgets::homeMune_jump_TabWidget,this,&MediaPlayer::jumpTabWidget);
-    qDebug()<<"创建的插件MediaPlayer::init()"<<id<<ObjectName<<widgets->objectName();
-
+    connect(widgets, &MediaWidgets::homeMune_jump_TabWidget, this, &MediaPlayer::jumpTabWidget);
+    qDebug() << "创建的插件MediaPlayer::init()" << id << ObjectName << widgets->objectName();
 }
 
-QString MediaPlayer::getObjectNane()
+QObject *MediaPlayer::instance()
+{
+    return this;
+}
+
+bool MediaPlayer::Start()
+{
+    qDebug() << "bool MediaPlayer::Start()"<<ObjectName;
+//    AppSettings.setPath(QSettings::IniFormat, QSettings::UserScope,"config/"+ObjectName+".ini");
+    return true;
+}
+
+QString MediaPlayer::getObjectNane() const
 {
     return ObjectName;
 }
 
 QWidget *MediaPlayer::getWidgetByName(QString name)
 {
-    return widgets->findChild<QWidget*>(name);
+    if(name == HomeMain_TabWidgetName){
+        return videoWindow;
+    }
+    return widgets->findChild<QWidget *>(name);
 }
 
 bool MediaPlayer::widgetReturnAfterRemoved(QWidget *widget)
 {
-    qDebug()<<"MediaPlayer::widgetReturnAfterRemoved(QWidget *widget)";
-    if(widget){
+    qDebug() << "MediaPlayer::widgetReturnAfterRemoved(QWidget *widget)";
+    if (widget)
+    {
         // 使用setParent方法将子QWidget重新设置为父QWidget的子对象
         widget->setParent(widgets);
-//        widgets.layout()->addWidget(widget);//方法会奔溃
+        //        widgets.layout()->addWidget(widget);//方法会奔溃
         return true;
     }
     return false;
@@ -45,30 +65,31 @@ QWidget *MediaPlayer::getHomeTiler()
 
 void MediaPlayer::quit()
 {
-    qDebug()<<"MediaPlayer::quit()保存，再删除";
-    widgets->quit(); //先保存，再删除，
+    qDebug() << "MediaPlayer::quit()";
+    widgets->quit(); // 先保存，再删除，
+    videoWindow->quit();
+    AppSettings.sync(); // 未保存的写入永久存储，并加载在此期间由其他应用程序的更改的任何设置。通常会在析构和事件循环中调用。
 }
 
 void MediaPlayer::jumpTabWidget()
 {
-    emit signalShowMainWidget(id,HomeMain_TabWidgetName);
+    emit signalShowMainWidget(id, HomeMain_TabWidgetName);
 }
 
 /*
  * name：一个字符串，表示要创建的插件的名称。
  * spec：一个字符串，表示插件的规范或配置。
-*/
-//QObject *MediaPlayer::create(const QString &name, const QString &spec)
+ */
+// QObject *MediaPlayer::create(const QString &name, const QString &spec)
 //{
-//    qDebug()<<"MediaPlayer::create(const QString &"<<name<<spec;
+//     qDebug()<<"MediaPlayer::create(const QString &"<<name<<spec;
 ////    static_assert(false, "You need to implement this function");//用于指定要检查的条件。如果条件为假，则会触发编译错误
 //    return this;
 //}
 
 MediaPlayer::~MediaPlayer()
 {
-    delete widgets;
-    qDebug()<<"MediaPlayer::~MediaPlayer()";
+//    delete widgets;
+//    delete videoWindow;
+    qDebug() << "MediaPlayer::~MediaPlayer()";
 }
-
-
--- MediaPlayer/mediaplayer.h
+++ MediaPlayer/mediaplayer.h
@@ -3,42 +3,41 @@
 
 #include "PluginInterface.h"
 #include "ui/MediaWidgets.h"
+#include "videowindow.h"
 
 //! [0] 定义插件接入
-class MediaPlayer : public QObject,PluginInterface
+class MediaPlayer : public QObject, PluginInterface
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID PluginInterface_iid FILE "MediaPlayer.json")
-    //接口是一种纯虚函数的集合，用于定义一组共享的行为或功能,需要在类的声明中的public部分使用它
+    // 接口是一种纯虚函数的集合，用于定义一组共享的行为或功能,需要在类的声明中的public部分使用它
     Q_INTERFACES(PluginInterface)
 
 public:
     explicit MediaPlayer(QObject *parent = nullptr);
     ~MediaPlayer();
-     void init();
-//    void initialize() override;
+    void init();
+    //    void initialize() override;
 public:
-    QString getObjectNane() override;
+    QObject *instance() override;
+    bool Start() override;
+
+    QString getObjectNane() const override;
     QWidget *getWidgetByName(QString name) override;
-    bool widgetReturnAfterRemoved(QWidget* widget) override;
+    bool widgetReturnAfterRemoved(QWidget *widget) override;
     QWidget *getHomeTiler() override;
 
-
     MediaWidgets *widgets;
+    VideoWindow *videoWindow;
 
 signals:
-    void signalShowMainWidget(int index,QString name) override;
+    void signalShowMainWidget(int index, QString name) override;
 
 public slots:
     void quit() override;
     void jumpTabWidget();
-private:
 
+private:
 };
 
 #endif // MEDIAPLAYER_H
-
-
-
-
-
